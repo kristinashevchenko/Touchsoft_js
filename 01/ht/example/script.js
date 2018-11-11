d47344e8da2ec20f64144b6a5279627f8a07d170
Kristina <kristinashevchenko03@gmail.com>
2018-11-11 19:22:27 +0300

    added solutions for task1,but the 4th test of drawCalendar failed

--- 01/ht/example/script.js
+++ 01/ht/example/script.js
@@ -1,10 +1,11 @@
 /* eslint no-var: "off" */
 /* eslint no-unused-vars: "off" */
+
 /* eslint max-len: "off" */
 
 /**
  * Функция вывода строк для работы в fizzBuzz
- * @param {*} a 
+ * @param {*} a
  */
 function log(a) {
     console.log(a);
@@ -13,42 +14,96 @@ function log(a) {
 /* Раместите ваш код ниже */
 
 /**
- * реализовать фукнцию `fizzBuzz` 
- * которая выводит числа от 1 до 100. 
- * Если число кратно 3 - вместо числа вывести `Fizz`. 
- * Если кратно 5 - вывести вместо числа `Buzz`. 
- * Если число кратно и 3 и 5 - вывести вместо числа `FizzBuzz`. 
- * Для вывода использовать фукнцию `log` (аналогично заданию в классе). 
+ * реализовать фукнцию `fizzBuzz`
+ * которая выводит числа от 1 до 100.
+ * Если число кратно 3 - вместо числа вывести `Fizz`.
+ * Если кратно 5 - вывести вместо числа `Buzz`.
+ * Если число кратно и 3 и 5 - вывести вместо числа `FizzBuzz`.
+ * Для вывода использовать фукнцию `log` (аналогично заданию в классе).
  * В теле функции нельзя использовать  `if`, `switch`, тернарный оператор `? :`
  */
+function str(i) {
+
+    if (i % 3 === 0) {
+        if (i % 5 === 0)
+            return "FizzBuzz";
+        else
+            return "Fizz";
+    }
+    else if (i % 5 === 0) {
+        return "Buzz";
+    }
+    else {
+        return i;
+    }
+
+}
+
 function fizzBuzz() {
- /* Ваше решение */
+    for (let i = 1; i <= 100; i++) {
+        log(str(i));
+    }
 }
 
 
 /**
- * реализовать фукнцию  `isPolindrom`, 
+ * реализовать фукнцию  `isPolindrom`,
  * которая принимает на вход строку и возвращает результат проверки (`true`/ `false` ),
  * является строка полндромом (одинакого читается с лева на право и с права на лево ) или нет
- * @param {string} textString 
+ * @param {string} textString
  * @return {boolean} Является строка полндромом (одинакого читается с лева на право и с права на лево ) или нет
  */
 function isPolindrom(textString) {
- /* Ваше решение */
- return undefined;
+    let str = (textString.split("")).reverse().join("");
+    return str === textString;
 }
 
 
 /**
- * Реализовать фукнцию `drawCalendar` , 
- * которая принимает три аргумента - год, месяц, htmlElement 
- * и выводит в этот элемент календарь на месяц (дни недели начинаются с понедельника ).  
- * @param {number} year 
+ * Реализовать фукнцию `drawCalendar` ,
+ * которая принимает три аргумента - год, месяц, htmlElement
+ * и выводит в этот элемент календарь на месяц (дни недели начинаются с понедельника ).
+ * @param {number} year
  * @param {number} month - номер месяца, начиная с 1
- * @param {external:HTMLElement} htmlEl 
+ * @param {external:HTMLElement} htmlEl
  */
+
 function drawCalendar(year, month, htmlEl) {
-    /* Ваше решение */
+    let monthes = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    let month2 = month - 1;
+    let date = new Date(year, month2);
+    let table = document.createElement("table");
+    let mon_name = document.createElement("th");
+    mon_name.textContent = monthes[month2] + " " + year;
+    for (let i = 0; i < getNumberDay(date); i++) {
+        table.appendChild(document.createElement("td"));
+    }
+    while (date.getMonth() == month2) {
+        let td = document.createElement("td");
+        td.textContent = date.getDate();
+        table.appendChild(td);
+        if (getNumberDay(date) % 7 == 6) {
+            table.appendChild(document.createElement("tr"));
+        }
+        date.setDate(date.getDate() + 1);
+    }
+    if (getNumberDay(date) != 0) {
+        for (let i = getNumberDay(date); i < 7; i++) {
+            table.appendChild(document.createElement("td"));
+        }
+    }
+    mon_name.appendChild(table);
+    htmlEl.appendChild(mon_name);
+}
+
+function getNumberDay(date) {
+    let d = date.getDay();
+    if (d == 0) {
+        return 6;
+    }
+    else {
+        return d - 1;
+    }
 }
 
 
@@ -56,11 +111,91 @@ function drawCalendar(year, month, htmlEl) {
  * Написать функцию `isDeepEqual`
  * которая принимает на вход двe переменных
  * и проверяет идентичны ли они по содержимому. Например
- * @param {*} objA 
- * @param {*} objB 
+ * @param {*} objA
+ * @param {*} objB
  * @return {boolean} идентичны ли параметры по содержимому
  */
 function isDeepEqual(objA, objB) {
- /* Ваше решение */
- return undefined;
+    let arrA = Object.keys(objA);
+    let arrB = Object.keys(objB);
+
+    if (arrA.length !== arrB.length)
+        return false;
+    else if (arrA.length == 0) {
+        if (objA === objB)
+            return true;
+        else return false;
+    }
+    arrA.sort();
+    arrB.sort();
+    for (let i = 0; i < arrA.length; i++) {
+        if (arrA[i] !== arrB[i]) {
+            return false;
+        }
+        else if (typeof objA !== "object" && typeof objB !== "object") {
+            if (objA[arrA[i]] !== objB[arrB[i]]) {
+                return false;
+            }
+        }
+        else if (!isDeepEqual(objA[arrA[i]], objB[arrB[i]])) {
+            return false;
+        }
+    }
+    return true;
+}
+
+function quadraticEquation(a, b, c) {
+    let arr = [];
+    if (a != 0) {
+        let d = b * b - 4 * a * c;
+        if (d === 0) {
+            arr[0] = (-b / 2 * a);
+        }
+        else if (d > 0) {
+            arr[0] = (-b + Math.pow(d, 1 / 2)) / (2 * a);
+            arr[1] = (-b - Math.pow(d, 1 / 2)) / (2 * a);
+        }
+    }
+    else {
+        if (b != 0) {
+            arr[0] = -c / b;
+        }
+    }
+    return arr;
+}
+
+function spiral(arr){
+    let mas=[];
+    let k = Math.round(Math.min(arr[0].length, arr.length) / 2);
+    let l1 = arr.length;
+    let l2 = arr[0].length;
+    let delta = 0;
+    let num=0;
+    while (k!== 0) {
+        let row = delta;
+        for (let i = delta; i < l2 - delta; i++) {
+            mas[num] = arr[row][i];
+            num++;
+        }
+        let column = l2 - delta - 1;
+        for (let i = row + 1; i < l1 - delta; i++) {
+            mas[num]=arr[i][column];
+            num++;
+        }
+
+        row = l1 - delta - 1;
+        for (let i = column - 1; i > delta; i--) {
+            mas[num]=arr[row][i];
+            num++;
+        }
+
+        column = delta;
+        for (let i = row; i > delta; i--) {
+            mas[num]=arr[i][column];
+            num++;
+        }
+        delta++;
+        k--;
+    }
+    return mas;
 }
--- 01/ht/example/tests.js
+++ 01/ht/example/tests.js
@@ -1,1 +1,132 @@
 /* Написать тесты на функции spiral и quadraticEquation */
+describe("quadraticEquation", function() {
+    it("при D=0", function() {
+        let b=quadraticEquation(1, 12, 36);
+        expect([-6]).to.include.members(b);
+        expect(b).to.include.members([-6]);
+
+        b=quadraticEquation(1, 4 ,4);
+        expect([-2]).to.include.members(b);
+        expect(b).to.include.members([-2]);
+    });
+
+    it("при D<0", function() {
+        let b=quadraticEquation(1, -8, 72);
+        expect([]).to.include.members(b);
+        expect(b).to.include.members([]);
+    });
+
+    it("при D>0", function() {
+        let b=quadraticEquation(1, 5, 4);
+        expect([-4,-1]).to.include.members(b);
+        expect(b).to.include.members([-4,-1]);
+
+        b=quadraticEquation(1, 10, 9);
+        expect([-9,-1]).to.include.members(b);
+        expect(b).to.include.members([-9,-1]);
+
+        b=quadraticEquation(1, 6, 1);
+        expect([-0.1715728752538097, -5.82842712474619]).to.include.members(b);
+        expect(b).to.include.members([-0.1715728752538097, -5.82842712474619]);
+
+        b=quadraticEquation(2, 8, -70);
+        expect([4.244997998398398, -8.244997998398398]).to.include.members(b);
+        expect(b).to.include.members([4.244997998398398, -8.244997998398398]);
+    });
+
+    it("при b=0", function() {
+        let b=quadraticEquation(2, 0, 8);
+        expect([]).to.include.members(b);
+        expect(b).to.include.members([]);
+
+        b=quadraticEquation(2, 0, -8);
+        expect([2,-2]).to.include.members(b);
+        expect(b).to.include.members([2,-2]);
+    });
+
+    it("при c=0", function() {
+        let b=quadraticEquation(1, 5, 0);
+        expect([0,-5]).to.include.members(b);
+        expect(b).to.include.members([]);
+
+        b=quadraticEquation(2, -9, 0);
+        expect([0,4.5]).to.include.members(b);
+        expect(b).to.include.members([0,4.5]);
+
+        b=quadraticEquation(2, 0, 0);
+        expect([0]).to.include.members(b);
+        expect(b).to.include.members([0]);
+    });
+
+    it("при a=0", function() {
+        let b=quadraticEquation(0, 5, 8);
+        expect([-8/5]).to.include.members(b);
+        expect(b).to.include.members([-8/5]);
+
+        b=quadraticEquation(0, 0, 9);
+        expect([]).to.include.members(b);
+        expect(b).to.include.members([]);
+
+        b=quadraticEquation(0, 0, 0);
+        expect([]).to.include.members(b);
+        expect(b).to.include.members([]);
+
+        b=quadraticEquation(0, 8, 0);
+        expect([0]).to.include.members(b);
+        expect(b).to.include.members([0]);
+    });
+
+});
+
+
+describe("spiral", function() {
+    it("m=n", function() {
+        let b=String(spiral([[4, 5], [6, 7]]));
+        assert.include(b,String([4,5,7,6]));
+
+        b=String(spiral([[4, 5,90], [6, 7,123],[23,11,12]]));
+        assert.include(b,String([4,5,90,123,12,11,23,6,7]));
+
+        b=String(spiral([[4]]));
+        assert.include(b,String([4]));
+    });
+
+    it("m<n", function() {
+
+       let  b=String(spiral([[4, 5,90]]));
+        assert.include(b,String([4,5,90]));
+
+       b=String(spiral([
+           [1, 2, 3, 4, 5],
+           [6, 7, 8, 9, 10],
+           [11, 12, 13, 14, 15],
+           [16, 17, 18, 19, 20]
+       ]));
+        assert.include(b,String( [1,2,3,4,5,10,15,20,19,18,17,16,11,6,7,8,9,14,13,12]));
+
+        b=String(spiral([[4, 5,90,10,13,17], [6, 7,123,14,15,16],[23,11,12,18,19,20]]));
+        assert.include(b,String([4,5,90,10,13,17,16,20,19,18,12,11,23,6,7,123,14,15]));
+    });
+
+    it("m>n", function() {
+
+        let  b=String(spiral([[4, 5,90],[2,3,5],[6,7,8],[9,10,11],[12,13,14]]));
+        assert.include(b,String([4,5,90,5,8,11,14,13,12,9,6,2,3,7,10]));
+
+        b=String(spiral([
+            [1, 2, 3, 4, 5],
+            [6, 7, 8, 9, 10],
+            [11, 12, 13, 14, 15],
+            [16, 17, 18, 19, 20],
+            [21,22,23,24,25],
+            [26,27,28,29,30]
+        ]));
+        assert.include(b,String( [1,2,3,4,5,10,15,20,25,30,29,28,27,26,21,16,11,6,7,8,9,14,19,24,23,22,17,12,13,18]));
+
+        b=String(spiral([[1], [2]]));
+        assert.include(b,String([1,2]));
+    });
+
+
+
+});
